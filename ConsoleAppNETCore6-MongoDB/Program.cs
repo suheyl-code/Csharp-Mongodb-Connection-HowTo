90404b791ee32a2ab6108e5a42a406099a6a2b0e
Suheyl Esadli <79663047+suheyl-code@users.noreply.github.com>
2022-05-08 20:27:17 +0300

    project updated

--- ConsoleAppNETCore6-MongoDB/Program.cs
+++ ConsoleAppNETCore6-MongoDB/Program.cs
@@ -1,2 +1,21 @@
 ﻿
-string connectionString = "mongodb://localhost:27017";
+using MongoDB.Driver;
+
+string connectionString = "mongodb://127.0.0.1:27017";
+string databaseName = "sample_db";
+string collectionName = "people";
+
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(databaseName);
+var collection = db.GetCollection<PersonModel>(collectionName);
+
+/// Creating and adding one record to collection
+//var person = new PersonModel { FirstName = "Alex", LastName = "Johnson" };
+//await collection.InsertOneAsync(person);
+
+/// Reading elements from database collection
+var results = await collection.FindAsync(_ => true);
+foreach (var result in results.ToList())
+{
+    Console.WriteLine($"{result.Id}: {result.FirstName} {result.LastName}");
+}
